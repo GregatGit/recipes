7bb773c00b57b1f314d4a4f7a9cc57995d2f54d7
Greg Duncan <gregdd@outlook.com>
2017-10-17 14:51:44 +1000

    use our jason

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,8 +1,5 @@
+import recipes from '../data/recipes.json'
+
 export default function () {
-  return [
-    { name: 'cake', ingredients: 4 },
-    { name: 'bread', ingredients: 3 },
-    { name: 'beer', ingredients: 7 },
-    { name: 'burger', ingredients: 5 }
-  ]
+  return recipes
 }
